4b71aabcb2a5e9c48a453c8456672060ba1b3bc5
Hans M. Boron <hansmateusboron@gmail.com>
2022-01-25 19:54:56 -0300

    add cronometer button

---
+++ lib/components/cronometer_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CronometerButton extends StatelessWidget {
+  final String text;
+  final IconData icon;
+
+  const CronometerButton({Key? key, required this.text, required this.icon})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.black,
+        padding: const EdgeInsets.symmetric(
+          vertical: 18,
+          horizontal: 26,
+        ),
+        textStyle: const TextStyle(
+          fontSize: 25,
+        ),
+      ),
+      onPressed: () {},
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Icon(
+              icon,
+              size: 34,
+            ),
+          ),
+          Text(text),
+        ],
+      ),
+    );
+  }
+}
